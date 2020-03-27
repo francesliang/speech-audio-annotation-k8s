8cd270b8bcbb84d389cbeafd31f50174f95170af
Xin <frances.xliang@gmail.com>
2020-03-27 12:11:44 +1100

    Update deployment script to include docker image build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+echo "Starting minikube..."
+
+minikube start
+
+echo "Setting docker env..."
+
+eval $(minikube docker-env)
+
+echo "Building system images for both backend and frontend..."
+
+pushd ./speech-audio-annotation
+docker build -t speech-audio-annotation_flask:latest .
+popd
+
+pushd ./speech-audio-annotation-ui
+docker build -t speech-audio-annotation-ui_react:latest .
+popd
+
 echo "Createing the flask deployment and service..."
 
 kubectl create -f ./kubernetes/flask-deployment.yml
